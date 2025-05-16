7542169aa1b2a9e1bb6dda518f44c0fb0f8f70c6
David Dzakpasu <davedjakpasoo@gmail.com>
2025-05-16 14:50:25 +0930

    Fixed typo in train.py file

--- train.py
+++ train.py
@@ -47,7 +47,7 @@ f1 = f1_score(y_test, predictions, average="macro")
 
 print("Accuracy: ", str(round(accuracy, 2) * 100) + "%", "F1: ", round(f1, 2))
 
-with open("Results/metrics.text", "w") as outfile:
+with open("Results/metrics.txt", "w") as outfile:
     outfile.write(f"\nAccuracy = {accuracy}, F1 Score = {f1}")
 
 cm = confusion_matrix(y_test, predictions, labels=pipe.classes_)
